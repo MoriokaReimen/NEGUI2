3631a197d615e33328e8790dd60ebf2bf6827bc9
MoriokaReimen <kei.nakata.job@gmail.com>
2023-09-16 06:19:10 +0900

    [update] enttを追加

--- cmake/External.cmake
+++ cmake/External.cmake
@@ -2,39 +2,39 @@ include_guard(GLOBAL)
 
 include(FetchContent)
 
-##################################################
+# #################################################
 # google test
-##################################################
+# #################################################
 FetchContent_Declare(
   googletest
   GIT_REPOSITORY https://github.com/google/googletest.git
-  GIT_TAG        v1.13.0
+  GIT_TAG v1.13.0
 )
 FetchContent_MakeAvailable(googletest)
 
-##################################################
+# #################################################
 # spdlog
-##################################################
+# #################################################
 FetchContent_Declare(
   spdlog
   GIT_REPOSITORY https://github.com/gabime/spdlog.git
-  GIT_TAG        v1.11.0
+  GIT_TAG v1.11.0
 )
 FetchContent_MakeAvailable(spdlog)
 
-##################################################
+# #################################################
 # vulkan headers
-##################################################
+# #################################################
 FetchContent_Declare(
   vulkan_headers
   GIT_REPOSITORY https://github.com/KhronosGroup/Vulkan-Headers.git
-  GIT_TAG        v1.3.261
+  GIT_TAG v1.3.261
 )
 FetchContent_MakeAvailable(vulkan_headers)
 
-##################################################
+# #################################################
 # glfw
-##################################################
+# #################################################
 set(GLFW_BUILD_EXAMPLES OFF)
 set(GLFW_BUILD_TESTS OFF)
 set(GLFW_BUILD_DOCS OFF)
@@ -42,13 +42,13 @@ set(GLFW_INSTALL OFF)
 FetchContent_Declare(
   glfw
   GIT_REPOSITORY https://github.com/glfw/glfw.git
-  GIT_TAG        3.3.8
+  GIT_TAG 3.3.8
 )
 FetchContent_MakeAvailable(glfw)
 
-##################################################
+# #################################################
 # Eigen
-##################################################
+# #################################################
 FetchContent_Declare(
   Eigen
   GIT_REPOSITORY https://gitlab.com/libeigen/eigen.git
@@ -60,46 +60,47 @@ set(EIGEN_BUILD_DOC OFF)
 set(EIGEN_BUILD_PKGCONFIG OFF)
 FetchContent_MakeAvailable(Eigen)
 
-##################################################
+# #################################################
 # glslang
-##################################################
+# #################################################
 FetchContent_Declare(
   glslang
   GIT_REPOSITORY https://github.com/KhronosGroup/glslang.git
-  GIT_TAG        sdk-1.3.250.0
+  GIT_TAG sdk-1.3.250.0
 )
 FetchContent_MakeAvailable(glslang)
 
-##################################################
+# #################################################
 # vma
-##################################################
+# #################################################
 FetchContent_Declare(
   vma
   GIT_REPOSITORY https://github.com/GPUOpen-LibrariesAndSDKs/VulkanMemoryAllocator.git
-  GIT_TAG        master
+  GIT_TAG master
 )
 FetchContent_MakeAvailable(vma)
 
-##################################################
+# #################################################
 # imgui
-##################################################
+# #################################################
 FetchContent_Declare(
   imgui
   GIT_REPOSITORY https://github.com/ocornut/imgui.git
   GIT_TAG docking
-  )
-  FetchContent_GetProperties(imgui)
-if (NOT imgui_POPULATED)
+)
+FetchContent_GetProperties(imgui)
+
+if(NOT imgui_POPULATED)
   find_package(glfw3 REQUIRED)
   FetchContent_Populate(imgui)
 
-    add_library(imgui STATIC
+  add_library(imgui STATIC
     ${imgui_SOURCE_DIR}/imgui.cpp
     ${imgui_SOURCE_DIR}/imgui_draw.cpp
     ${imgui_SOURCE_DIR}/imgui_demo.cpp
     ${imgui_SOURCE_DIR}/imgui_tables.cpp
     ${imgui_SOURCE_DIR}/imgui_widgets.cpp
-  
+
     ${imgui_SOURCE_DIR}/backends/imgui_impl_glfw.cpp
     ${imgui_SOURCE_DIR}/backends/imgui_impl_vulkan.cpp
   )
@@ -107,46 +108,48 @@ if (NOT imgui_POPULATED)
   target_compile_definitions(imgui PUBLIC IMGUI_IMPL_VULKAN_NO_PROTOTYPES)
   add_library(imgui::imgui ALIAS imgui)
   target_include_directories(imgui
-  PUBLIC
+    PUBLIC
     ${imgui_SOURCE_DIR}
     ${imgui_SOURCE_DIR}/backends
   )
-endif ()
+endif()
 
-##################################################
+# #################################################
 # implot
-##################################################
+# #################################################
 FetchContent_Declare(
   implot
   GIT_REPOSITORY https://github.com/epezent/implot.git
   GIT_TAG v0.15
-  )
-  FetchContent_GetProperties(implot)
-if (NOT implot_POPULATED)
+)
+FetchContent_GetProperties(implot)
+
+if(NOT implot_POPULATED)
   FetchContent_Populate(implot)
   add_library(implot STATIC
-  ${implot_SOURCE_DIR}/implot.cpp
-  ${implot_SOURCE_DIR}/implot_demo.cpp
-  ${implot_SOURCE_DIR}/implot_items.cpp
+    ${implot_SOURCE_DIR}/implot.cpp
+    ${implot_SOURCE_DIR}/implot_demo.cpp
+    ${implot_SOURCE_DIR}/implot_items.cpp
   )
   target_link_libraries(implot PUBLIC imgui::imgui)
   add_library(imgui::implot ALIAS implot)
   target_include_directories(implot
-  PUBLIC
+    PUBLIC
     ${implot_SOURCE_DIR}
   )
-endif ()
+endif()
 
-##################################################
+# #################################################
 # imguizmo
-##################################################
+# #################################################
 FetchContent_Declare(
   imguizmo
   GIT_REPOSITORY https://github.com/CedricGuillemet/ImGuizmo.git
   GIT_TAG master
-  )
-  FetchContent_GetProperties(imguizmo)
-if (NOT imguizmo_POPULATED)
+)
+FetchContent_GetProperties(imguizmo)
+
+if(NOT imguizmo_POPULATED)
   FetchContent_Populate(imguizmo)
   add_library(imguizmo STATIC
     ${imguizmo_SOURCE_DIR}/GraphEditor.cpp
@@ -158,21 +161,22 @@ if (NOT imguizmo_POPULATED)
   target_link_libraries(imguizmo PUBLIC imgui::imgui)
   add_library(imgui::imguizmo ALIAS imguizmo)
   target_include_directories(imguizmo
-  PUBLIC
+    PUBLIC
     ${imguizmo_SOURCE_DIR}
   )
-endif ()
+endif()
 
-##################################################
+# #################################################
 # colortextedit
-##################################################
+# #################################################
 FetchContent_Declare(
   colortextedit
   GIT_REPOSITORY https://github.com/BalazsJako/ImGuiColorTextEdit.git
   GIT_TAG master
-  )
-  FetchContent_GetProperties(colortextedit)
-if (NOT colortextedit_POPULATED)
+)
+FetchContent_GetProperties(colortextedit)
+
+if(NOT colortextedit_POPULATED)
   FetchContent_Populate(colortextedit)
   add_library(colortextedit STATIC
     ${colortextedit_SOURCE_DIR}/TextEditor.cpp
@@ -180,26 +184,37 @@ if (NOT colortextedit_POPULATED)
   target_link_libraries(colortextedit PUBLIC imgui::imgui)
   add_library(imgui::colortextedit ALIAS colortextedit)
   target_include_directories(colortextedit
-  PUBLIC
+    PUBLIC
     ${colortextedit_SOURCE_DIR}
   )
-endif ()
+endif()
 
-##################################################
+# #################################################
 # stb
-##################################################
+# #################################################
 FetchContent_Declare(
   stb
   GIT_REPOSITORY https://github.com/nothings/stb.git
   GIT_TAG master
-  )
-  FetchContent_GetProperties(stb)
-if (NOT stb_POPULATED)
+)
+FetchContent_GetProperties(stb)
+
+if(NOT stb_POPULATED)
   FetchContent_Populate(stb)
   add_library(stb INTERFACE)
   add_library(stb::stb ALIAS stb)
   target_include_directories(stb
-  INTERFACE
+    INTERFACE
     ${stb_SOURCE_DIR}
   )
-endif ()
+endif()
+
+# #################################################
+# entt
+# #################################################
+FetchContent_Declare(
+  entt
+  GIT_REPOSITORY https://github.com/skypjack/entt.git
+  GIT_TAG v3.12.2
+)
+FetchContent_MakeAvailable(entt)
