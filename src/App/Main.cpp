52407cf4b0adf9a232dc4edf8d943b710e233dfb
MoriokaReimen <kei.nakata.job@gmail.com>
2023-09-17 21:49:52 +0900

    [update] Widget実装

--- src/App/Main.cpp
+++ src/App/Main.cpp
@@ -1,17 +1,9 @@
 #include "NEGUI2/Core/Core.hpp"
-#include "NEGUI2/Ui/UiDemo.hpp"
-#include "NEGUI2/Ui/PlotDemo.hpp"
-#include "NEGUI2/Ui/TextEditDemo.hpp"
-#include "NEGUI2/Ui/TextureDemo.hpp"
-#include "NEGUI2/3D/Triangle.hpp"
-#include "NEGUI2/3D/Coordinate.hpp"
-#include "NEGUI2/3D/IDisplayObject.hpp"
 #include <cstdlib>
 #include <memory>
-#include "NEGUI2/3D/Camera.hpp"
-#include "NEGUI2/3D/Grid.hpp"
 #include <spdlog/spdlog.h>
 #include "Scene.hpp"
+#include "Widget.hpp"
 #include <entt/entt.hpp>
 
 int main(int argc, char** argv)
@@ -19,13 +11,14 @@ int main(int argc, char** argv)
     auto registry = std::make_shared<entt::registry>();
     auto& core =NEGUI2::Core::get_instance();
     App::Scene scene(registry);
-    NEGUI2::TextureDemo demo2;
+    App::Widget widget(registry);
     scene.init();
+    widget.init();
 
     while(!core.should_close())
     {
         scene.update();
-        demo2.update();
+        widget.update();
         core.update();
     }
     core.wait_idle();
--- src/App/Widget.hpp
+++ src/App/Widget.hpp
@@ -1,16 +1,18 @@
 #ifndef _WIDGET_HPP
 #define _WIDGET_HPP
 #include "IModule.hpp"
+#include <vulkan/vulkan.h>
 
 namespace App
 {
     class Widget : public IModule
     {
+        VkDescriptorSet texture_id_;
         public:
         Widget(std::shared_ptr<entt::registry> registry);
         virtual ~Widget() override;
-        virtual void init() = 0;
-        virtual void update() = 0;
+        virtual void init() override;
+        virtual void update() override;
     };
 
 }
